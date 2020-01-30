6efb8dd54ad80789f6d357d8496df8c896129801
Takdanai <rookiehome7@gmail.com>
2020-01-30 21:44:01 +0700

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes, ExtraOptions } from '@angular/router';
+
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileModule } from './profile/profile.module';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProfileComponent
+  }
+];
+
+const config: ExtraOptions = {
+  useHash: true,
+};
+
 
 @NgModule({
   declarations: [
@@ -10,7 +28,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes, config),
+    AppRoutingModule,
+    HttpClientModule,
+    ProfileModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
